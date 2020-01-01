022d2ca0cc6cb0c06220f1714b78e9c76765261b
ermias <ermias@gmail.com>
2020-01-01 10:00:01 +0300

    kkkkkkkk

---
+++ jpp/src/tconv.java
@@ -0,0 +1,23 @@
+public class tconv {
+
+	private int hand;
+	private int attribute;
+	private int name;
+	private int attribute2;
+
+	public void getName() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setName(int name) {
+		this.name = name;
+	}
+
+	public void getAttribute2() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setAttribute2(int attribute2) {
+		this.attribute2 = attribute2;
+	}
+}
